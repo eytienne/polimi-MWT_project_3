621c0bd0c97fbeb9f29c1c8fda4b4b527c8383fb
eytienne <arabeyre.etienne@gmail.com>
2022-08-12 15:38:15 +0200

    update word-count

--- back/src/word-count.cpp
+++ back/src/word-count.cpp
@@ -32,6 +32,14 @@ operator<<(ostream &os,
 	return os;
 }
 
+vector<string> get_word_tokens(string input) {
+	std::regex rgx("((?:\\w|[-_])+)");
+	std::sregex_token_iterator begin(input.begin(), input.end(), rgx, 1), end;
+	vector<string> v;
+	std::copy(begin, end, std::back_inserter(v));
+	return v;
+}
+
 static int comm_rank, comm_size;
 
 #define LOG(values...)                                                         \
@@ -52,13 +60,14 @@ void log(auto const &rem, auto const &values) {
 
 #define OFFSETOF(type, field) ((unsigned long)&(((type *)0)->field))
 
-vector<string> get_word_tokens(string input) {
-	std::regex rgx("((?:\\w|[-_])+)");
-	std::sregex_token_iterator begin(input.begin(), input.end(), rgx, 1), end;
-	vector<string> v;
-	std::copy(begin, end, std::back_inserter(v));
-	return v;
+vector<int> get_displs(const int *recvcounts) {
+	auto displs = vector<int>(comm_size);
+	for (size_t i = 1; i < comm_size; i++) {
+		displs[i] = displs[i - 1] + recvcounts[i - 1];
+	}
+	return displs;
 }
+
 
 int main(int argc, char **argv) {
 	std::ios_base::sync_with_stdio(false);
@@ -87,21 +96,17 @@ int main(int argc, char **argv) {
 	map<string, int> table;
 	for (int i = 0; i * comm_size + comm_rank < argc - 1; i++) {
 		string url = *(argv + i * comm_size + comm_rank + 1);
-		auto r = cpr::Get(cpr::Url{url});
-		if (r.status_code < 200 || r.status_code > 299) {
+		auto response = cpr::Get(cpr::Url{url});
+		if (response.status_code < 200 || response.status_code > 299) {
 			continue;
 		}
-		istringstream in(r.text);
-		std::string line;
-		while (std::getline(in, line)) {
-			auto words = get_word_tokens(line);
-			for (auto _word : words) {
-				auto word = _word;
-				std::transform(word.begin(), word.end(), word.begin(),
-							   [](char cc) { return std::tolower(cc); });
-				table[word]++;
-			}
-		}
+        auto words = get_word_tokens(response.text);
+        for (auto _word : words) {
+            auto word = _word;
+            std::transform(word.begin(), word.end(), word.begin(),
+                            [](char cc) { return std::tolower(cc); });
+            table[word]++;
+        }
 	}
 	auto beg = table.begin();
 	auto partitions =
@@ -129,22 +134,22 @@ int main(int argc, char **argv) {
 	auto recvbuf = new word_count[recvbuf_size];
 	for (int rank = 0; rank < comm_size; rank++) {
 		auto partition = partitions[rank];
-		auto displs = new int[comm_size]{0};
-		for (size_t i = 1; i < comm_size; i++) {
-			displs[i] = displs[i - 1] + recvbuf_sizes[i - 1];
-		}
+		auto displs = get_displs(recvbuf_sizes);
 		MPI_Gatherv(partition.data(), partition.size(), mpi_word_count, recvbuf,
-					recvbuf_sizes, displs, mpi_word_count, rank,
+					recvbuf_sizes, displs.data(), mpi_word_count, rank,
 					MPI_COMM_WORLD);
-		delete displs;
 	}
 
-	table.clear();
-	for (int i = 0; i < recvbuf_size; i++) {
-		auto item = recvbuf[i];
-		table[item.word] += item.count;
-	}
-	delete recvbuf;
+	auto fill_table = [](map<string, int> &table, word_count *recvbuf, int recvbuf_size) {
+		table.clear();
+		for (int i = 0; i < recvbuf_size; i++) {
+			auto item = recvbuf[i];
+			table[item.word] += item.count;
+		}
+		delete recvbuf;
+	};
+
+	fill_table(table, recvbuf, recvbuf_size);
 
 	auto completePartitionSize = table.size();
 	auto completePartition = vector<word_count>(completePartitionSize);
@@ -163,20 +168,12 @@ int main(int argc, char **argv) {
 		recvbuf_size = std::accumulate(recvbuf_sizes, recvbuf_sizes + comm_size, 0);
 		recvbuf = new word_count[recvbuf_size];
 	}
-	auto displs = new int[comm_size]{0};
-	for (size_t i = 1; i < comm_size; i++) {
-		displs[i] = displs[i - 1] + recvbuf_sizes[i - 1];
-	}
-	MPI_Gatherv(completePartition.data(), completePartition.size(), mpi_word_count, recvbuf, recvbuf_sizes, displs, mpi_word_count, ROOT, MPI_COMM_WORLD);
+	auto displs = get_displs(recvbuf_sizes);
+	MPI_Gatherv(completePartition.data(), completePartition.size(), mpi_word_count, recvbuf, recvbuf_sizes, displs.data(), mpi_word_count, ROOT, MPI_COMM_WORLD);
 	if (comm_rank == ROOT) {
-		table.clear();
-		for (int i = 0; i < recvbuf_size; i++) {
-			auto item = recvbuf[i];
-			table[item.word] += item.count;
-		}
+		fill_table(table, recvbuf, recvbuf_size);
 		log("recvbuf", table);
 	}
-	delete displs;
 	delete recvbuf_sizes;
 
 	MPI_Type_free(&mpi_word_count);
